a01c5c61ddf09e2d02ea03816c828e109b2101df
Tomáš Zemanovič <tomas@heliax.dev>
2025-11-20 14:00:09 +0100

    turn off broken LLVM lint

--- lib/compiler-llvm/src/lib.rs
+++ lib/compiler-llvm/src/lib.rs
@@ -6,10 +6,10 @@
     unused_unsafe,
     unreachable_patterns
 )]
-#![cfg_attr(
-    all(not(target_os = "windows"), not(target_arch = "aarch64")),
-    deny(dead_code)
-)]
+// #![cfg_attr(
+//     all(not(target_os = "windows"), not(target_arch = "aarch64")),
+//     deny(dead_code)
+// )]
 #![doc(html_favicon_url = "https://wasmer.io/images/icons/favicon-32x32.png")]
 #![doc(html_logo_url = "https://github.com/wasmerio.png?size=200")]
 #![cfg_attr(docsrs, feature(doc_cfg, doc_auto_cfg))]
